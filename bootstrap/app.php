b6dc007505fef1272d1a6b1d45e2457d74b9f4cd
Tarik Amine <tarik.amine@stellatelecom.com>
2020-07-30 14:26:23 +0200

    add mongodb library

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,9 +23,9 @@ $app = new Laravel\Lumen\Application(
     dirname(__DIR__)
 );
 
-// $app->withFacades();
+$app->withFacades();
 
-// $app->withEloquent();
+$app->withEloquent();
 
 /*
 |--------------------------------------------------------------------------
@@ -96,6 +96,7 @@ $app->configure('graphql');
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 $app->register(Rebing\GraphQL\GraphQLLumenServiceProvider::class);
+$app->register(Jenssegers\Mongodb\MongodbServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
